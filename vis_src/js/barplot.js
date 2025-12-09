bfcc86b5531dc79a81f2f40b4e5d1daa7d6523c7
Ricky Curry <rickycurry@gmail.com>
2025-12-09 14:44:55 -0800

    Bar plot working, other than tooltip positioning.

--- vis_src/js/barplot.js
+++ vis_src/js/barplot.js
@@ -19,23 +19,25 @@ export class Barplot {
         
         this.candidates = _candidateData.filter(d => d.type_elxn === 0);
         this.candidatesGroupedByParliament = d3.group(this.candidates, d => d.parliament);
-        this.majorPartiesLookup = new Map();
-        this.occupationsLookup = new Map();
-        this.provincesLookup = new Map();
+        this.majorPartiesLookup = _majorPartiesLookup;
+        this.occupationsLookup = _occupationsLookup;
+        this.provincesLookup = _provincesLookup;
         this.currentParliament = this.config.currentParliament;
         this.selectedFeds = new Set();
-        // this.aoi = null;
         this.quantAttr = "Winner and\nseat share";
         this.aggregationAttr = "party_major_group_cleaned";
-        _majorPartiesLookup.forEach(d => this.majorPartiesLookup.set(d.id, d.party));
-        _occupationsLookup.forEach(d => this.occupationsLookup.set(d.id, d.occupation));
-        _provincesLookup.forEach(d => this.provincesLookup.set(d.id, d.province));
         this.initVis();
     }
 
     changeAOI(attr) {
-        this.quantAttr = attr;
-        this.updateVis();
+        let vis = this;
+        vis.quantAttr = attr;
+        if (attr === "Non-male" || attr === "Indigenous") {
+            vis.yAxis.tickFormat(d3.format(".0%"));
+        } else {
+            vis.yAxis.tickFormat(null);
+        }
+        vis.updateVis();
     }
 
     changeParliament(newParliament, newSelectedGeography) {
@@ -67,19 +69,24 @@ export class Barplot {
             // double-subtract margins...
             .range([0, vis.width - vis.config.margin.left - vis.config.margin.right])
             .padding(0.15);
+        console.log(vis.xScale.bandwidth());
 
         // scale for subgroups (all vs win)
         vis.xSub = d3.scaleBand()
             .domain(['all','win'])
-            .padding(0.05);
+            .padding(0.1);
+        console.log(vis.xSub.bandwidth());
 
         // Initialize axes
         vis.xAxis = d3.axisBottom(vis.xScale)
             .tickSizeOuter(0);
 
         vis.yAxis = d3.axisLeft(vis.yScale)
-            .tickSizeOuter(0)
-            .tickFormat(d3.format(".0%"));
+            .tickSizeOuter(0);
+
+        vis.colourScale = d3.scaleOrdinal()
+            .domain(vis.xSub.domain())
+            .range(["#03b6fc", "#fcb103"]);
 
         vis.svg = d3.select(`#${vis.config.parentElement}`)
             .append('svg')
@@ -112,48 +119,44 @@ export class Barplot {
 
     renderVis() {
         let vis = this;
-        vis.chart.append("g")
-            .attr("transform", `translate(0, ${vis.height})`)
-            .call(vis.xAxis);
-        vis.chart.append("g")
-            .call(vis.yAxis);
+
+        vis.xScale.domain(vis.data.map(d => d[0]));
+        vis.xSub.range([0, vis.xScale.bandwidth()]);
+        vis.yScale.domain([0, d3.max(vis.data.map(d => Math.max(d[1].all, d[1].win)))]).nice();
 
         // update axes
         vis.xAxisG.call(vis.xAxis);
         vis.yAxisG.call(vis.yAxis);
 
-        // one group per party
-        const groups = vis.chart.selectAll(".bar-group")
-            .data(vis.data, d => d[0]);
-
-        const groupsEnter = groups.join(
-            enter => enter.append("g")
-                .attr("class", "bar-group")
-                .attr("transform", d => `translate(${vis.xScale(d[0])},0)`),
-            update => update
-                .attr("transform", d => `translate(${vis.xScale(d[0])},0)`),
-            exit => exit.remove()
-        );
-
-        // two rects per group: all candidates and winners
-        groupsEnter.selectAll("rect")
-            .data(d => (['all','win'].map(k => ({k, v: k === 'all' ? d[1] : d[2]}))))
-            .join("rect")
-            .attr("class", d => `bar bar-${d.k}`)
-            .attr("x", d => vis.xSub(d.k))
-            .attr("width", vis.xSub.bandwidth())
-            .attr("y", d => vis.yScale(d.v != null ? d.v : 0))
-            .attr("height", d => vis.height - vis.yScale(d.v != null ? d.v : 0))
-            .attr("fill", d => d.k === 'all' ? "steelblue" : "orange");
-
-            // update bars 
-            groupsEnter.selectAll("rect")
-            .transition().duration(250)
-            .attr("x", d => vis.xSub(d.k))
-            .attr("width", vis.xSub.bandwidth())
-            .attr("y", d => vis.yScale(d.v != null ? d.v : 0))
-            .attr("height", d => vis.height - vis.yScale(d.v != null ? d.v : 0))
-            .attr("fill", d => d.k === 'all' ? "steelblue" : "orange");
+        vis.chart.selectAll('.group-g')
+            .data(vis.data, d => d[0])
+            .join('g')
+            .attr("transform", d => `translate(${vis.xScale(d[0])}, 0)`)
+            .attr("debugname", d => d[0])
+            .classed('group-g', true)
+            .classed('barplot-bar', true)
+            .selectAll('rect')
+            .data(d => [{agg: d[0], group: 'all', val: d[1].all}, {agg: d[0], group: 'win', val: d[1].win}], d => d.group)
+            .join('rect')
+            .attr('x', d => vis.xSub(d.group))
+            .attr('y', d => vis.yScale(d.val))
+            .attr('width', vis.xSub.bandwidth())
+            .attr('height', d => vis.yScale(0) - vis.yScale(d.val))
+            .attr('fill', d => vis.colourScale(d.group));
+            // .on("click", (event, d) => {
+            //     vis.selectedRowLabel = d.rowLabel;
+            //     vis.changeAOICallback(d.rowLabel);
+            //     vis.renderVis();
+            // })
+        vis.chart.selectAll('rect')
+            .on("mousemove", (event, d) => {
+                d3.select('#map-tooltip')
+                    .style('display', 'block')
+                    .style('left', (event.pageX + vis.config.tooltipPadding) + 'px')
+                    .style('bottom', (window.innerHeight - event.pageY + vis.config.tooltipPadding) + 'px')
+                    .html(`<div class="tooltip-body">${d.val}</div>`);
+                })
+            .on('mouseleave', () => { d3.select('#map-tooltip').style('display', 'none'); });
     }
 
     filterCandidates() {
@@ -171,46 +174,62 @@ export class Barplot {
         switch (vis.quantAttr) {
             case "Vote share":
                 const totalVotes = d3.sum(vis.filteredCandidates, d => d.votes);
-                const totalWinnerVotes = d3.sum(vis.filteredCandidates.filter(d => d.elected), d => d.votes);
-                vis.candidateData = d3.rollups(vis.filteredCandidates, D => d3.sum(D, d => d.votes) / totalVotes, d => d[vis.aggregationAttr]);
-                vis.winnerData = d3.rollups(vis.filteredCandidates.filter(d => d.elected), D => d3.sum(D, d => d.votes) / totalWinnerVotes, d => d[vis.aggregationAttr]);
+                // const totalWinnerVotes = d3.sum(vis.filteredCandidates.filter(d => d.elected), d => d.votes);
+                vis.data = d3.rollups(vis.filteredCandidates, D => {
+                    return {all: d3.sum(D, d => d.votes) / totalVotes, win: d3.sum(D.filter(d => d.elected), d => d.votes) / totalVotes};
+                }, d => d[vis.aggregationAttr]);
                 break;
             case "Age":
-                vis.candidateData = d3.rollups(vis.filteredCandidates, D => d3.mean(D, d => d.age), d => d[vis.aggregationAttr]);
-                vis.winnerData = d3.rollups(vis.filteredCandidates.filter(d => d.elected), D => d3.mean(D, d => d.age), d => d[vis.aggregationAttr]);
+                vis.data = d3.rollups(vis.filteredCandidates, D => {
+                    return {all: d3.mean(D, d => d.age_at_election), win: d3.mean(D.filter(d => d.elected), d => d.age_at_election)};
+                }, d => d[vis.aggregationAttr]);
                 break;
             case "Non-male":
-                vis.candidateData = d3.rollups(vis.filteredCandidates, D => D.filter(d => d.gender !== 'M').length / D.length, d => d[vis.aggregationAttr]);
-                vis.winnerData = d3.rollups(vis.filteredCandidates.filter(d => d.elected), D => D.filter(d => d.gender !== 'M').length / D.length, d => d[vis.aggregationAttr]);
+                vis.data = d3.rollups(vis.filteredCandidates, D => {
+                    const electedOnly = D.filter(d => d.elected);
+                    return {
+                        all: D.filter(d => d.gender !== 'M').length / D.length, 
+                        win: electedOnly.filter(d => d.gender !== 'M').length / electedOnly.length
+                    };
+                }, d => d[vis.aggregationAttr]);
                 break;
             case "Indigenous":
-                vis.candidateData = d3.rollups(vis.filteredCandidates, D => D.filter(d => d.indigenous_origins).length / D.length, d => d[vis.aggregationAttr]);
-                vis.winnerData = d3.rollups(vis.filteredCandidates.filter(d => d.elected), D => D.filter(d => d.indigenous_origins).length / D.length, d => d[vis.aggregationAttr]);
+                vis.data = d3.rollups(vis.filteredCandidates, D => {
+                    const electedOnly = D.filter(d => d.elected);
+                    return {
+                        all: D.filter(d => d.indigenousorigins).length / D.length, 
+                        win: electedOnly.filter(d => d.indigenousorigins).length / electedOnly.length
+                    };
+                }, d => d[vis.aggregationAttr]);
                 break;
             case "Count":
             case "Winner and\nseat share": 
             default:
-                vis.candidateData = d3.rollups(vis.filteredCandidates, D => D.length, d => d[vis.aggregationAttr]);
-                vis.winnerData = d3.rollups(vis.filteredCandidates.filter(d => d.elected), D => D.length, d => d[vis.aggregationAttr]);
+                vis.data = d3.rollups(vis.filteredCandidates, D => {
+                    return {all: D.length, win: D.filter(d => d.elected).length};
+                }, d => d[vis.aggregationAttr]);
                 break;
         }
-        console.log(vis.candidateData);
-        console.log(vis.winnerData);
-    }
-
-    updateData() {
-        let vis = this;
-        vis.filterCandidates();
-
-        // default to 'outcome' when quantAttr is not set
-        const attr = vis.quantAttr || "outcome";
-        vis.data = [];
-
-        // use the already-filtered candidate list for the currently selected RO/parliament
-        const primaryElectionCandidates = vis.filteredCandidates || vis.candidates;
-        const winnersCandidates = (primaryElectionCandidates || []).filter(c => +c.elected === 1);
-
-        let mapall = new Map();
-        let mapwinners = new Map();
+        
+        vis.data.sort((a, b) => a[0] - b[0]);
+        vis.data = vis.data.map(d => {
+            let mappingArray, mappingKey;
+            switch (vis.aggregationAttr) {
+                case "province":
+                    mappingArray = vis.provincesLookup;
+                    mappingKey = "province";
+                    break;
+                case "party_major_group_cleaned":
+                    mappingArray = vis.majorPartiesLookup;
+                    mappingKey = "party";
+                    break;
+                case "occupation_category":
+                    mappingArray = vis.occupationsLookup;
+                    mappingKey = "occupation";
+                    break;
+            }
+            return [mappingArray[d[0]][mappingKey], d[1]]
+        });
+        console.log(vis.data);
     }
 }
